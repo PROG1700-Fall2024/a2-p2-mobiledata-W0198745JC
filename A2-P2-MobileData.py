c68be81f383b622f2e6851d4feee4e737f5f9e3a
W0198745JC <w0198745@campus.nscc.ca>
2024-10-15 11:40:08 -0300

    main code and pseudo code done

--- A2-P2-MobileData.py
+++ A2-P2-MobileData.py
@@ -5,13 +5,27 @@
 # Note, it is not a cumulative charge; your program will figure out which single range the usage falls into, 
 # then calculate the cost based on that range’s cost. 
 
-#Student #:     
-#Student Name:  
+#Student #:W0198745     
+#Student Name:  Jenille Cheney
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
+#input Mb Useage:
+    mb=int(input("Enter Data Usage (Mb) : "))
+#Elif statement for mb useage
+    if mb <= 200:                        # thought all variables needed "" but just string does
+        cost= 20.00
+    elif mb == 200 and mb <=500:         #orginally had or but quickly switched to and
+        cost= .105*mb
+    elif mb >=500 and mb <=1000:
+        cost= .110*mb
+    else:
+        cost=118.00
 
-
+#calculate cost  ( This was done within the if/elif statement)
+    
+#print cost 
+    print("Total Charge is ${0:.2f}".format(cost))
 
 
 
